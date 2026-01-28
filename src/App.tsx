c0212e301e6f87648edc4c8bf1ac2b840ccc0585
dmfshove <funnymoments610@gmail.com>
2026-01-28 17:07:20 +0100

    docs(interface): change usecase_diagram files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import routes from "@/config/routes.ts"
-import type { RouteType } from '@/config/routes.ts'
 
 function App() {
 
