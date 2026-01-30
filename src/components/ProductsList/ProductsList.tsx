57b7dddc7f5666db9d87a5490ca3e3ec9f246123
dmfshove <funnymoments610@gmail.com>
2026-01-30 10:06:26 +0100

    feat(products/product list): write html

---
+++ src/components/ProductsList/ProductsList.tsx
@@ -0,0 +1,17 @@
+import styles from "./ProductsList.module.scss"
+
+function ProductsList() {
+    return (
+        <section className={`${styles.products_list_content} section`}>
+            <div className={`${styles.container} container`}>
+                <div className={`${styles.row} row`}>
+                    <div className={`${styles.column} column`}>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ProductsList
