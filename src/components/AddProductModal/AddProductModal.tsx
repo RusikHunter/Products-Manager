8f58933ccf59654e4a17f1316fbe00df5735e27a
dmfshove <funnymoments610@gmail.com>
2026-03-10 17:01:05 +0100

    feat(control panel): implement the addition of a product

--- src/components/AddProductModal/AddProductModal.tsx
+++ src/components/AddProductModal/AddProductModal.tsx
@@ -1,4 +1,7 @@
+import { useRef } from "react"
 import styles from "./AddProductModal.module.scss"
+import { useAppDispatch } from "@/hooks/useAppDispatch"
+import { addProduct } from "@/store/reducers/productsReducer"
 
 type AddProductModalProps = {
     isOpen: boolean
@@ -6,26 +9,50 @@ type AddProductModalProps = {
 }
 
 function AddProductModal({ isOpen, setIsOpen }: AddProductModalProps) {
-    console.log(isOpen)
+    const dispatch = useAppDispatch()
+    const form = useRef<HTMLFormElement>(null)
+
+    function addNewProduct() {
+        if (!form.current) return
+
+        const formData = new FormData(form.current)
+
+        const productData: Record<string, any> = {}
+
+        for (const [key, value] of formData.entries()) {
+            if (!value) return
+
+            productData[key] = value
+        }
+
+        dispatch(addProduct(productData))
+
+        setIsOpen(false)
+    }
 
     return (
         <dialog className={styles.dialog} open={isOpen}>
             <p className={styles.title}>Add new product</p>
 
-            <form className={styles.form}>
+            <form ref={form} className={styles.form}>
                 <label className={styles.label} htmlFor="addProductModalTitle">Title:</label>
-                <input className={`${styles.input} input`} id="addProductModalTitle" type="text" />
+                <input className={`${styles.input} input`} id="addProductModalTitle" name="title" type="text" />
 
                 <label className={styles.label} htmlFor="addProductModalDescription">Description:</label>
-                <textarea className={`${styles.textarea} textarea`} id="addProductModalDescription"></textarea>
+                <textarea className={`${styles.textarea} textarea`} name="description" id="addProductModalDescription"></textarea>
 
                 <label className={styles.label} htmlFor="addProductModalCount">Count:</label>
-                <input className={`${styles.input} input`} id="addProductModalCount" type="number" />
+                <input className={`${styles.input} input`} name="count" id="addProductModalCount" type="number" />
 
                 <label className={styles.label} htmlFor="addProductModalPrice">Price:</label>
-                <input className={`${styles.input} input`} id="addProductModalPrice" type="number" />
+                <input className={`${styles.input} input`} name="price" id="addProductModalPrice" type="number" />
 
-                <button className={`${styles.button__submit} button__control`} type="button">Confirm</button>
+                <button
+                    className={`${styles.button__submit} button__control`}
+                    onClick={() => addNewProduct()}
+                    type="button">
+                    Confirm
+                </button>
             </form>
 
             <button className={styles.button__close} onClick={() => setIsOpen(false)}>X</button>
