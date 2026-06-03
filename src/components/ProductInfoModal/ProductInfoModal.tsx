8b4b5b25f32532661af4dd66dfd43d3ac6a00eb3
dmfshove <funnymoments610@gmail.com>
2026-06-03 22:15:37 +0200

    feat(productinfomodal): show info about product on modal window.

--- src/components/ProductInfoModal/ProductInfoModal.tsx
+++ src/components/ProductInfoModal/ProductInfoModal.tsx
@@ -2,6 +2,7 @@ import styles from "./ProductInfoModal.module.scss"
 import { useAppDispatch } from "@/hooks/useAppDispatch"
 import { setIsCurrentProductForInfoAvailable } from "@/store/reducers/productsReducer"
 import { useAppSelector } from "@/hooks/useAppSelector"
+import type { ProductType } from "@/@types/ProductType"
 
 function ProductInfoModal() {
     const dispatch = useAppDispatch()
@@ -10,24 +11,25 @@ function ProductInfoModal() {
         dispatch(setIsCurrentProductForInfoAvailable(false))
     }
 
-    const isOpen = useAppSelector(state => state.productsReducer.isCurrentProductForInfoAvailable)
+    const isOpen: boolean = useAppSelector(state => state.productsReducer.isCurrentProductForInfoAvailable)
+    const currentProduct: ProductType | null = useAppSelector(state => state.productsReducer.currentProductForInfo)
 
     return (
         <dialog open={isOpen} className={`${styles.dialog} dialog`}>
             <div className={styles.dialog__content}>
                 <button className={`${styles.button__close} button__control`} onClick={handleCloseClick}>X</button>
 
-                <p className={styles.id}>ID: 123</p>
+                <p className={styles.id}>ID: {currentProduct?.id}</p>
 
-                <h1 className={styles.title}>Banana</h1>
+                <h1 className={styles.title}>{currentProduct?.title}</h1>
 
-                <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae harum sint est assumenda porro quos numquam optio deserunt aliquid animi provident excepturi ipsam unde sapiente, alias rem blanditiis. Aliquid, a.</p>
+                <p className={styles.description}>{currentProduct?.description}</p>
 
-                <p className={styles.count}>Count: 999</p>
+                <p className={styles.count}>Count: {currentProduct?.count}</p>
 
-                <p className={styles.production__date}>Production date: 30.01.2026</p>
+                <p className={styles.production__date}>Production date: {currentProduct?.production_date}</p>
 
-                <p className={styles.price}>Price: 19.99$</p>
+                <p className={styles.price}>Price: {currentProduct?.price}$</p>
 
                 <button className={`${styles.button__change} button__control`}>Change</button>
 
