dd0726aabaac06acf2ff5d02596a69de5883d2a7
dmfshove <funnymoments610@gmail.com>
2026-06-03 22:51:08 +0200

    styles(changeproductmodal): write SCSS styles

--- src/components/ChangeProductModal/ChangeProductModal.tsx
+++ src/components/ChangeProductModal/ChangeProductModal.tsx
@@ -10,17 +10,18 @@ function ChangeProductModal() {
 
                 <form className={styles.form}>
                     <label htmlFor="changeModalTitleInput" className={styles.label}>Title: </label>
-                    <input type="text" id="changeModalTitleInput" className={styles.input} />
+                    <input type="text" id="changeModalTitleInput" className={`${styles.input} input`} />
 
                     <label htmlFor="changeModalDescriptionInput" className={styles.label}>Description: </label>
-                    <input type="text" id="changeModalDescriptionInput" className={styles.input} />
+                    <input type="text" id="changeModalDescriptionInput" className={`${styles.input} input`} />
 
                     <label htmlFor="changeModalCountInput" className={styles.label}>Count: </label>
-                    <input type="number" id="changeModalCountInput" className={styles.input} />
+                    <input type="number" id="changeModalCountInput" className={`${styles.input} input`} />
 
                     <label htmlFor="changeModalPriceInput" className={styles.label}>Price: </label>
-                    <input type="number" id="changeModalPriceInput" className={styles.input} />
+                    <input type="number" id="changeModalPriceInput" className={`${styles.input} input`} />
 
+                    <button className={`${styles.button__save} button__control`}>Save changes</button>
                 </form>
             </div>
         </dialog>
