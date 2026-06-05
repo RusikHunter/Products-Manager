de71337c16b2ab92851d351c6b69dfbab23484ee
dmfshove <funnymoments610@gmail.com>
2026-06-05 22:00:44 +0200

    feat(changeproductmodal): write a part of logic for changing products

--- src/components/ChangeProductModal/ChangeProductModal.tsx
+++ src/components/ChangeProductModal/ChangeProductModal.tsx
@@ -1,19 +1,41 @@
+import type { ProductType } from "@/@types/ProductType"
 import styles from "./ChangeProductModal.module.scss"
 import { useAppDispatch } from "@/hooks/useAppDispatch"
 import { useAppSelector } from "@/hooks/useAppSelector"
 import { setIsChangeProductModalOpen } from "@/store/reducers/productsReducer"
+import { useState } from "react"
 
 function ChangeProductModal() {
     const dispatch = useAppDispatch()
 
     const isOpen = useAppSelector(state => state.productsReducer.isChangeProductModalOpen)
+    const currentProduct: ProductType | null = useAppSelector(state => state.productsReducer.currentProductForInfo)
+
+    const [changedProduct, setChangedProduct] = useState<ProductType | null>(currentProduct)
+
+    if (!currentProduct) return
 
     const handleCloseClick = (): void => {
         dispatch(setIsChangeProductModalOpen(false))
     }
 
-    const handleSaveClick = (event: React.MouseEvent): void => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = event.target
+
+        // console.log(name)
+
+        setChangedProduct(prev => ({
+
+
+        }))
+    }
+
+    const handleSaveSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
         event.preventDefault()
+
+
+
+
         dispatch(setIsChangeProductModalOpen(false))
     }
 
@@ -24,20 +46,44 @@ function ChangeProductModal() {
 
                 <p className={styles.id}>ID: 123</p>
 
-                <form className={styles.form}>
+                <form className={styles.form} onSubmit={handleSaveSubmit}>
                     <label htmlFor="changeModalTitleInput" className={styles.label}>Title: </label>
-                    <input type="text" id="changeModalTitleInput" className={`${styles.input} input`} />
+                    <input
+                        type="text"
+                        id="changeModalTitleInput"
+                        className={`${styles.input} input`}
+                        value={changedProduct?.title ?? ""}
+                        onChange={handleChange}
+                    />
 
                     <label htmlFor="changeModalDescriptionInput" className={styles.label}>Description: </label>
-                    <input type="text" id="changeModalDescriptionInput" className={`${styles.input} input`} />
+                    <input
+                        type="text"
+                        id="changeModalDescriptionInput"
+                        className={`${styles.input} input`}
+                        value={changedProduct?.description ?? ""}
+                        onChange={handleChange}
+                    />
 
                     <label htmlFor="changeModalCountInput" className={styles.label}>Count: </label>
-                    <input type="number" id="changeModalCountInput" className={`${styles.input} input`} />
+                    <input
+                        type="number"
+                        id="changeModalCountInput"
+                        className={`${styles.input} input`}
+                        value={changedProduct?.count ?? ""}
+                        onChange={handleChange}
+                    />
 
                     <label htmlFor="changeModalPriceInput" className={styles.label}>Price: </label>
-                    <input type="number" id="changeModalPriceInput" className={`${styles.input} input`} />
+                    <input
+                        type="number"
+                        id="changeModalPriceInput"
+                        className={`${styles.input} input`}
+                        value={changedProduct?.price ?? ""}
+                        onChange={handleChange}
+                    />
 
-                    <button className={`${styles.button__save} button__control`} onClick={(event) => handleSaveClick(event)}>Save changes</button>
+                    <button type="submit" className={`${styles.button__save} button__control`}>Save changes</button>
                 </form>
             </div>
         </dialog>
